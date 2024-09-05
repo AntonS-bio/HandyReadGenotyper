3153c5e2d74cc1091836fb96d12878e4c09c5fa4
AntonS-bio <spadar.anton@gmail.com>
2024-09-05 10:47:08 +0100

    Version changes for release

--- scripts/classify.py
+++ scripts/classify.py
@@ -55,7 +55,7 @@ def classify(temp_dir):
     parser.add_argument('--organism_presence_cutoff', type=float,
                         help='Sample is reported as having target organism if at least this percentage  of non-transient amplicons have >10 reads. Values 0-100.', required=False, default=20.0)
    
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.22')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.24')
 
 
     try:
